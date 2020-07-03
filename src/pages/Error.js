d0949db8f2e32af3355978da6e41ffa2d7c85408
Henry Liu <hl3184@columbia.edu>
2020-07-03 00:11:31 -0400

    error page complete

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Error = () => {
-  return <div>error</div>;
+  return (
+    <section className="section section-error">
+      <h1 className="section__title">oops! it's a dead end</h1>
+      <Link to="/" className="btn btn-primary">
+        back home
+      </Link>
+    </section>
+  );
 };
 
 export default Error;
