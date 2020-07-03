2911432096ec7d7699503463c7b9afdf9025689c
Henry Liu <hl3184@columbia.edu>
2020-07-03 14:57:20 -0400

    install axios, set up the api request

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,59 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+//import API
+import axios from '../api';
+
+//import components
+import SearchForm from '../components/SearchForm';
+import CocktailList from '../components/CocktailList';
 
 const Home = () => {
-  return <div>Home</div>;
+  const [loading, setLoading] = useState(true);
+  const [searchTerm, setSearchTerm] = useState('d');
+  const [cocktails, setCocktails] = useState([]);
+
+  useEffect(() => {
+    async function getDrinks() {
+      try {
+        const response = await axios.get(`/search.php?s=${searchTerm}`);
+
+        const { drinks } = response.data;
+        if (drinks) {
+          const newCocktails = drinks.map((drink) => {
+            const {
+              idDrink,
+              strDrink,
+              strDrinkThumb,
+              strAlcoholic,
+              strGlass,
+            } = drink;
+            console.log(drink);
+            return {
+              id: idDrink,
+              name: strDrink,
+              image: strDrinkThumb,
+              info: strAlcoholic,
+              glass: strGlass,
+            };
+          });
+          setCocktails(newCocktails);
+        } else {
+          setCocktails([]);
+        }
+        setLoading(false);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getDrinks();
+  }, [searchTerm]);
+
+  return (
+    <main className="section section-home">
+      <SearchForm setSearchTerm={setSearchTerm} />
+      <CocktailList loading={loading} cocktails={cocktails} />
+    </main>
+  );
 };
 
 export default Home;
