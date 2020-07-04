c58e12aa66fdc66b2a81d2201d0befc57d51cdb2
Henry Liu <hl3184@columbia.edu>
2020-07-04 14:42:51 -0400

    navbar and homepage responsive design complete

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FaStream } from 'react-icons/fa';
 
 //import logo svg
 import logo from '../logo.svg';
@@ -10,9 +11,15 @@ import './Navbar.scss';
 const Navbar = () => {
   return (
     <nav className="nav">
-      <Link className="nav__brand" to="/">
-        <img src={logo} alt="brand" />
+      <Link to="/">
+        <img src={logo} alt="brand" className="nav__brand" />
       </Link>
+      <label htmlFor="show">
+        <FaStream className="nav__toggle" />
+      </label>
+
+      {/* checkbox used to toggle nav__items */}
+      <input type="checkbox" name="show" id="show" />
 
       <ul className="nav__items">
         <li className="nav__item">
